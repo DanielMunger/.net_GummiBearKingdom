df6cb1f8c27f3c67f31f8a101bb73e61e2d456f6
Daniel Munger <mungerda@gmail.com>
2017-02-10 11:19:00 -0800

    add create functionality; list of product on index page; routing.

--- src/GummiBearKingdom/Controllers/Products.cs
+++ src/GummiBearKingdom/Controllers/Products.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GummiBearKingdom.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 
 namespace GummiBearKingdom.Controllers
 {
     public class Products : Controller
     {
+        private GummiBearKingdomContext db = new GummiBearKingdomContext();
         public IActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
         }
         public IActionResult Details(int id)
         {
@@ -27,8 +33,21 @@ namespace GummiBearKingdom.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(string naem, int cost, string country, int picture)
+        public IActionResult Create(string name, int cost, string country, IFormFile picture)
         {
+            byte[] pictureArray = new byte[0];
+            if(picture.Length > 0)
+            {
+                using (var fileStream = picture.OpenReadStream())
+                using (var ms = new MemoryStream())
+                {
+                    fileStream.CopyTo(ms);
+                    pictureArray = ms.ToArray();
+                }
+            }
+            Product newProduct = new Product(name, cost, country, pictureArray);
+            db.Products.Add(newProduct);
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
         public IActionResult Edit()
