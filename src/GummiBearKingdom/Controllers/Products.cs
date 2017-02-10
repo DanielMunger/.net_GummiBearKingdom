46d28253bcd8e475a71e2f93a0e1914651ce31db
Daniel Munger <mungerda@gmail.com>
2017-02-10 10:53:51 -0800

    add views and controller methods for products.

---
+++ src/GummiBearKingdom/Controllers/Products.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GummiBearKingdom.Controllers
+{
+    public class Products : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Details()
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string naem, int cost, string country, int picture)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
